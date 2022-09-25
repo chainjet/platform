8ffc0a775e6b5a8b3dab15cbae421b051b328100
marian2js <marianomdq1@gmail.com>
2022-09-25 21:53:16 +0200

    fix: support cors when origin is the frontend endpoint

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,20 +1,20 @@
 import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
 import MongoStore from 'connect-mongo'
 import session from 'express-session'
 import { AppModule } from './app.module'
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     rawBody: true, // exposes req.rawBody which is needed by the discord hook
-    ...(process.env.NODE_ENV === 'development'
-      ? {
-          cors: {
-            origin: process.env.FRONTEND_ENDPOINT,
-            credentials: true,
-          },
-        }
-      : {}),
+    cors: {
+      origin: process.env.FRONTEND_ENDPOINT,
+      credentials: true,
+    },
   })
+  if (process.env.NODE_ENV !== 'development') {
+    app.set('trust proxy', 1)
+  }
   app.use(
     session({
       secret: process.env.SESSION_SECRET!,
