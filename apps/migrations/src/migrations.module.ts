4825bce738d7af5a557bf69d8acb379e55dcab47
marian2js <marianomdq1@gmail.com>
2022-10-27 13:55:05 +0200

    fix: remove deprecated migration

--- apps/migrations/src/migrations.module.ts
+++ apps/migrations/src/migrations.module.ts
@@ -7,11 +7,11 @@ import { IntegrationTriggersModule } from 'apps/api/src/integration-triggers/int
 import { IntegrationsModule } from 'apps/api/src/integrations/integrations.module'
 import { UsersModule } from 'apps/api/src/users/users.module'
 import { WorkflowActionsModule } from 'apps/api/src/workflow-actions/workflow-actions.module'
+import { WorkflowRunsModule } from 'apps/api/src/workflow-runs/workflow-runs.module'
 import { WorkflowTriggersModule } from 'apps/api/src/workflow-triggers/workflow-triggers.module'
 import { WorkflowsModule } from 'apps/api/src/workflows/workflows.module'
 import { mongoForRoot } from '../../../libs/common/src/utils/mongodb'
 import { MigrationsService } from './migrations.service'
-import { Migration0001 } from './migrations/0001-add-iusername-prop'
 import { Migration0002 } from './migrations/0002-remove-integration'
 
 @Module({
@@ -27,8 +27,9 @@ import { Migration0002 } from './migrations/0002-remove-integration'
     WorkflowsModule,
     WorkflowTriggersModule,
     WorkflowActionsModule,
+    WorkflowRunsModule,
   ],
   controllers: [],
-  providers: [MigrationsService, Migration0001, Migration0002],
+  providers: [MigrationsService, Migration0002],
 })
 export class MigrationsModule {}
