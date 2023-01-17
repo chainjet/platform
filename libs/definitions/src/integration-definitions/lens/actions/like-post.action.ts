4fe6519e09d2eacf54f56684ebe897927fb95640
marian2js <marianomdq1@gmail.com>
2023-01-17 13:58:47 -0300

    feat: add action to like lens posts

---
+++ libs/definitions/src/integration-definitions/lens/actions/like-post.action.ts
@@ -0,0 +1,55 @@
+import { AuthenticationError } from '@app/common/errors/authentication-error'
+import { RunResponse } from '@app/definitions/definition'
+import { OperationOffChain } from '@app/definitions/opertion-offchain'
+import { sendGraphqlQuery } from '@app/definitions/utils/subgraph.utils'
+import { OperationRunOptions } from 'apps/runner/src/services/operation-runner.service'
+import { JSONSchema7 } from 'json-schema'
+import { getLensProfileId, refreshLensAccessToken } from '../lens.common'
+
+export class LikePostAction extends OperationOffChain {
+  key = 'likePost'
+  name = 'Like a post'
+  description = 'Lile a given post'
+  version = '1.0.0'
+
+  inputs: JSONSchema7 = {
+    required: ['publicationId'],
+    properties: {
+      publicationId: {
+        title: 'Publication ID',
+        type: 'string',
+      },
+    },
+  }
+  outputs: JSONSchema7 = {}
+
+  async run({ inputs, credentials, workflow }: OperationRunOptions): Promise<RunResponse> {
+    if (!credentials?.refreshToken || !credentials?.profileId) {
+      throw new AuthenticationError('Authentication is expired, please connect the profile again')
+    }
+    if (!inputs.publicationId) {
+      throw new Error('Publication ID is required')
+    }
+    const { profileId, refreshToken } = credentials
+    const refreshedCredentials = await refreshLensAccessToken(refreshToken)
+    if (!refreshedCredentials) {
+      throw new AuthenticationError('Authentication is expired, please connect the profile again')
+    }
+
+    const lensProfileId = await getLensProfileId(profileId)
+
+    const query = `
+    mutation AddReaction {
+      addReaction(request: { profileId: "${lensProfileId}", reaction: UPVOTE, publicationId: "${inputs.publicationId}" })
+    }`
+
+    const res = await sendGraphqlQuery('https://api.lens.dev/', query, {
+      'x-access-token': refreshedCredentials.accessToken,
+      origin: process.env.LORIGIN,
+    })
+    return {
+      outputs: {},
+      refreshedCredentials,
+    }
+  }
+}
--- libs/definitions/src/integration-definitions/lens/lens.definition.ts
+++ libs/definitions/src/integration-definitions/lens/lens.definition.ts
@@ -2,6 +2,7 @@ import { AuthenticationError } from '@app/common/errors/authentication-error'
 import { SingleIntegrationDefinition } from '@app/definitions/single-integration.definition'
 import { CreatePostAction } from './actions/create-post.action'
 import { FollowProfileAction } from './actions/follow-profile.action'
+import { LikePostAction } from './actions/like-post.action'
 import { refreshLensAccessToken } from './lens.common'
 import { NewFollowerTrigger } from './triggers/new-follower.trigger'
 import { NewMentionTrigger } from './triggers/new-mention.trigger'
@@ -13,7 +14,7 @@ export class LensDefinition extends SingleIntegrationDefinition {
   schemaUrl = null
 
   triggers = [new NewFollowerTrigger(), new NewPostTrigger(), new NewMentionTrigger()]
-  actions = [new FollowProfileAction(), new CreatePostAction()]
+  actions = [new FollowProfileAction(), new CreatePostAction(), new LikePostAction()]
 
   async refreshCredentials(credentials: Record<string, any>): Promise<Record<string, any>> {
     const refreshedCredentials = await refreshLensAccessToken(credentials.refreshToken)
