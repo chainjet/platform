380f481e5a9ffec7ad582899749e6b1b9485f7b3
marian2js <marianomdq1@gmail.com>
2023-02-07 20:50:22 -0300

    feat: calculate metrics for specific integrations

--- apps/platform-metrics/src/platform-metrics.service.ts
+++ apps/platform-metrics/src/platform-metrics.service.ts
@@ -1,11 +1,18 @@
 import { UserEventKey } from '@app/common/metrics/entities/user-event'
 import { UserEventService } from '@app/common/metrics/user-event.service'
+import { getDateFromObjectId } from '@app/common/utils/mongodb'
 import { Injectable, Logger } from '@nestjs/common'
+import { IntegrationActionService } from 'apps/api/src/integration-actions/services/integration-action.service'
+import { IntegrationTriggerService } from 'apps/api/src/integration-triggers/services/integration-trigger.service'
+import { IntegrationService } from 'apps/api/src/integrations/services/integration.service'
+import { UserService } from 'apps/api/src/users/services/user.service'
+import { WorkflowActionService } from 'apps/api/src/workflow-actions/services/workflow-action.service'
 import { WorkflowRunService } from 'apps/api/src/workflow-runs/services/workflow-run.service'
 import { WorkflowTriggerService } from 'apps/api/src/workflow-triggers/services/workflow-trigger.service'
 import { Workflow } from 'apps/api/src/workflows/entities/workflow'
 import { WorkflowService } from 'apps/api/src/workflows/services/workflow.service'
 import { WorkflowRunStatus } from 'generated/graphql'
+import { ObjectId } from 'mongodb'
 
 @Injectable()
 export class PlatformMetricsService {
@@ -13,9 +20,14 @@ export class PlatformMetricsService {
 
   constructor(
     private readonly workflowTriggerService: WorkflowTriggerService,
+    private readonly workflowActionService: WorkflowActionService,
     private readonly workflowRunService: WorkflowRunService,
     private readonly workflowService: WorkflowService,
     private readonly userEventService: UserEventService,
+    private readonly userService: UserService,
+    private readonly integrationService: IntegrationService,
+    private readonly integrationTriggerService: IntegrationTriggerService,
+    private readonly integrationActionService: IntegrationActionService,
   ) {}
 
   async onModuleInit() {
@@ -113,6 +125,90 @@ export class PlatformMetricsService {
     const sortedDashboardUses = new Map([...dashboardUses.entries()].sort((a, b) => b[1] - a[1]))
     for (const [dashboard, uses] of sortedDashboardUses.entries()) {
       console.log(`**${dashboard.name}** has been used ${uses} times`)
+    }
+  }
+
+  async countUniqueUsersAndWorkflowsForIntegration(key: string): Promise<{ users: number; workflows: number }> {
+    const lensIntegration = await this.integrationService.findOne({ key })
+    const integrationTriggers = await this.integrationTriggerService.find({
+      integration: lensIntegration!._id.toString(),
+    })
+    const integrationActions = await this.integrationActionService.find({
+      integration: lensIntegration!._id.toString(),
+    })
+
+    const workflowTriggers = await this.workflowTriggerService.find({
+      integrationTrigger: { $in: [...integrationTriggers.map((t) => t._id)] },
+    })
+    const workflowActions = await this.workflowActionService.find({
+      integrationAction: { $in: [...integrationActions.map((t) => t._id)] },
+    })
+
+    // unique workflow IDs
+    const workflowIds = new Set<string>()
+    for (const workflowTrigger of workflowTriggers) {
+      workflowIds.add(workflowTrigger.workflow.toString())
+    }
+    for (const workflowAction of workflowActions) {
+      workflowIds.add(workflowAction.workflow.toString())
+    }
+
+    // add all unique users and workflows with active triggers for the integration
+    const activeTriggers = await this.workflowTriggerService.find({
+      workflow: { $in: [...workflowIds] },
+      enabled: true,
+    })
+    const uniqueUsers = new Set<string>()
+    const uniqueWorkflows = new Set<string>()
+    for (const activeTrigger of activeTriggers) {
+      uniqueUsers.add(activeTrigger.owner.toString())
+      uniqueWorkflows.add(activeTrigger.workflow.toString())
+    }
+
+    // add all users and workflows without active trigger but with a completed run in the last 30 days
+    const nonActiveIds = [...workflowIds].reduce((acc, curr) => {
+      if (!activeTriggers.find((t) => t.workflow.toString() === curr)) {
+        acc.push(curr)
+      }
+      return acc
+    }, [] as string[])
+    const lastRuns = await this.workflowRunService.aggregateNative([
+      {
+        $match: {
+          workflow: { $in: [...nonActiveIds.map((id) => new ObjectId(id))] },
+          status: WorkflowRunStatus.completed,
+        },
+      },
+      {
+        $sort: {
+          _id: 1,
+        },
+      },
+      {
+        $group: {
+          _id: '$workflow',
+          latestRun: {
+            $first: '$$ROOT',
+          },
+        },
+      },
+      {
+        $replaceRoot: {
+          newRoot: '$latestRun',
+        },
+      },
+    ])
+    for (const lastRun of lastRuns) {
+      const runDate = getDateFromObjectId(lastRun._id)
+      if (runDate.getTime() > new Date().getTime() - 30 * 24 * 60 * 60 * 1000) {
+        uniqueUsers.add(lastRun.owner.toString())
+        uniqueWorkflows.add(lastRun.workflow.toString())
+      }
+    }
+
+    return {
+      users: uniqueUsers.size,
+      workflows: uniqueWorkflows.size,
     }
   }
 }
