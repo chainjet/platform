620b6fc5a51795c6ffa7308be41264a1f2c9653e
marian2js <marianomdq1@gmail.com>
2023-03-19 10:46:17 -0300

    feat: update plan configs

--- apps/api/src/users/config/plans.config.ts
+++ apps/api/src/users/config/plans.config.ts
@@ -1,31 +1,81 @@
 export interface PlanConfig {
-  name: string
+  key: string
   maxOperations: number
+  maxActiveWorkflows: number
+  minPollingInterval: number
+  features: {
+    executeWorkflowOnError: boolean
+  }
 }
 
-export const defaultPlan = 'early' // TODO make free plan the default
+export const defaultPlan = 'free'
 
 export const plansConfig: Record<string, PlanConfig> = {
   free: {
-    name: 'Free',
+    key: 'free',
     maxOperations: 10000,
+    maxActiveWorkflows: 3,
+    minPollingInterval: 60 * 15,
+    features: {
+      executeWorkflowOnError: false,
+    },
   },
-  // TODO
-  stripe_plan_id_1: {
-    name: 'Starter',
+  prod_NYG90VSEfU0TfQ: {
+    key: 'starter',
     maxOperations: 1e5,
+    maxActiveWorkflows: Infinity,
+    minPollingInterval: 60,
+    features: {
+      executeWorkflowOnError: true,
+    },
   },
-  // TODO
-  stripe_plan_id_2: {
-    name: 'Pro',
+  prod_NYGB1kY91pq5g6: {
+    key: 'pro',
     maxOperations: 3e5,
+    maxActiveWorkflows: Infinity,
+    minPollingInterval: 15,
+    features: {
+      executeWorkflowOnError: true,
+    },
+  },
+  prod_NYGCd7KzrCjd7Y: {
+    key: 'business',
+    maxOperations: 750000,
+    maxActiveWorkflows: Infinity,
+    minPollingInterval: 15,
+    features: {
+      executeWorkflowOnError: true,
+    },
   },
   internal: {
-    name: 'Internal',
+    key: 'internal',
     maxOperations: Infinity,
+    maxActiveWorkflows: Infinity,
+    minPollingInterval: 15,
+    features: {
+      executeWorkflowOnError: true,
+    },
   },
   early: {
-    name: 'Early Access',
+    key: 'early',
     maxOperations: Infinity,
+    maxActiveWorkflows: Infinity,
+    minPollingInterval: 60,
+    features: {
+      executeWorkflowOnError: true,
+    },
   },
+}
+
+// Plans on Stripe test mode
+if (process.env.NODE_ENV === 'development') {
+  // starter
+  plansConfig['prod_NXQOvZowLlwuaH'] = plansConfig['prod_NYG90VSEfU0TfQ']
+  delete plansConfig['prod_NYG90VSEfU0TfQ']
+  // pro
+  plansConfig['prod_NXsxne4WGLGVct'] = plansConfig['prod_NYGB1kY91pq5g6']
+  delete plansConfig['prod_NYGB1kY91pq5g6']
+  // business
+  plansConfig['prod_NXsy6KRk62T4yx'] = plansConfig['prod_NYGCd7KzrCjd7Y']
+  delete plansConfig['prod_NYGCd7KzrCjd7Y']
 }
