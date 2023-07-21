028a11328a13520d6a67ee3ac7807fc03c71269a
marian2js <marianomdq1@gmail.com>
2023-07-21 17:10:40 +0200

    feat: resolve wallet identities with airstack

--- libs/definitions/src/utils/address.utils.ts
+++ libs/definitions/src/utils/address.utils.ts
@@ -1,8 +1,24 @@
+import { getAddress } from 'ethers/lib/utils'
 import { EnsLib } from '../integration-definitions/ens/ens.lib'
+import { sendGraphqlQuery } from './subgraph.utils'
 
 export async function resolveAddressName(addressName: string) {
   if (!addressName) {
     throw new Error(`Missing address`)
+  }
+  if (addressName.includes('.')) {
+    const query = `
+    query {
+      Wallet(input: { identity: "${addressName}", blockchain: ethereum }) {
+        addresses
+      }
+    }`
+    const res = await sendGraphqlQuery('https://api.airstack.xyz/gql', query, {
+      Authorization: process.env.AIRSTACK_API_KEY,
+    })
+    if (res?.data?.Wallet?.addresses?.[0]) {
+      return getAddress(res.data.Wallet.addresses[0])
+    }
   }
   if (addressName.endsWith('.eth')) {
     return await EnsLib.getOwner(addressName)
