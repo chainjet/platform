1f49ba2715b65bdf112f4583ee759accb279ec8a
marian2js <marianomdq1@gmail.com>
2023-08-08 13:38:00 +0100

    feat: update plans config

--- apps/api/src/users/config/plans.config.ts
+++ apps/api/src/users/config/plans.config.ts
@@ -12,6 +12,7 @@ export interface PlanConfig {
 export const defaultPlan = 'free'
 
 export const plansConfig: Record<string, PlanConfig> = {
+  // TODO update plan limits
   free: {
     key: 'free',
     maxOperations: 10000,
@@ -22,20 +23,10 @@ export const plansConfig: Record<string, PlanConfig> = {
       executeWorkflowOnError: false,
     },
   },
-  prod_NbsBYZA9BxFF7r: {
+  prod_OPe3MZ5ynuDahA: {
     key: 'starter',
     maxOperations: 30000,
-    maxActiveWorkflows: 25,
-    minPollingInterval: 60 * 5,
-    maxTriggerItems: Infinity,
-    features: {
-      executeWorkflowOnError: true,
-    },
-  },
-  prod_NYG90VSEfU0TfQ: {
-    key: 'core',
-    maxOperations: 1e5,
-    maxActiveWorkflows: Infinity,
+    maxActiveWorkflows: 20,
     minPollingInterval: 60,
     maxTriggerItems: Infinity,
     features: {
@@ -72,6 +63,17 @@ export const plansConfig: Record<string, PlanConfig> = {
       executeWorkflowOnError: true,
     },
   },
+  // this plan is deprecated
+  prod_NYG90VSEfU0TfQ: {
+    key: 'core',
+    maxOperations: 1e5,
+    maxActiveWorkflows: Infinity,
+    minPollingInterval: 60,
+    maxTriggerItems: Infinity,
+    features: {
+      executeWorkflowOnError: true,
+    },
+  },
   early: {
     key: 'early',
     maxOperations: Infinity,
@@ -87,11 +89,8 @@ export const plansConfig: Record<string, PlanConfig> = {
 // Plans on Stripe test mode
 if (process.env.NODE_ENV === 'development') {
   // starter
-  plansConfig['prod_NbsFKhWJ6PMBpx'] = plansConfig['prod_NbsBYZA9BxFF7r']
-  delete plansConfig['prod_NbsBYZA9BxFF7r']
-  // core
-  plansConfig['prod_NXQOvZowLlwuaH'] = plansConfig['prod_NYG90VSEfU0TfQ']
-  delete plansConfig['prod_NYG90VSEfU0TfQ']
+  plansConfig['prod_NbsFKhWJ6PMBpx'] = plansConfig['prod_OPe3MZ5ynuDahA']
+  delete plansConfig['prod_OPe3MZ5ynuDahA']
   // pro
   plansConfig['prod_NXsxne4WGLGVct'] = plansConfig['prod_NYGB1kY91pq5g6']
   delete plansConfig['prod_NYGB1kY91pq5g6']
