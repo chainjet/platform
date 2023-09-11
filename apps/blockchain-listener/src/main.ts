8e495094e99099e42e894f1bf90051da584b16dd
marian2js <marianomdq1@gmail.com>
2023-09-11 12:13:20 +0200

    chore: remove deprecated blockchain listener

--- apps/blockchain-listener/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core'
-import { BlockchainListenerModule } from './blockchain-listener.module'
-
-async function bootstrap() {
-  const app = await NestFactory.create(BlockchainListenerModule)
-  await app.listen(process.env.PORT ?? 8300)
-}
-bootstrap()
