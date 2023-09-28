7827e365b4d7d7ff142dcf885c6d78a9ac7da5b6
marian2js <marianomdq1@gmail.com>
2023-09-27 21:40:43 -0400

    feat: make the default subscription group non-deletable

--- apps/api/src/chat/services/subscription-group.service.ts
+++ apps/api/src/chat/services/subscription-group.service.ts
@@ -1,5 +1,6 @@
 import { BaseService } from '@app/common/base/base.service'
-import { Injectable, Logger } from '@nestjs/common'
+import { BadRequestException, Injectable, Logger, NotFoundException } from '@nestjs/common'
+import { DeleteOneOptions } from '@ptc-org/nestjs-query-core'
 import { ReturnModelType } from '@typegoose/typegoose'
 import { InjectModel } from 'nestjs-typegoose'
 import { SubscriptionGroup } from '../entities/subscription-group'
@@ -10,5 +11,16 @@ export class SubscriptionGroupService extends BaseService<SubscriptionGroup> {
 
   constructor(@InjectModel(SubscriptionGroup) protected readonly model: ReturnModelType<typeof SubscriptionGroup>) {
     super(model)
+  }
+
+  async deleteOne(id: string, opts?: DeleteOneOptions<SubscriptionGroup> | undefined): Promise<SubscriptionGroup> {
+    const subscriptionGroup = await this.model.findById(id)
+    if (!subscriptionGroup) {
+      throw new NotFoundException('Subscription group not found')
+    }
+    if (subscriptionGroup.defaultGroup) {
+      throw new BadRequestException('Cannot delete default subscription group')
+    }
+    return super.deleteOne(id, opts)
   }
 }
