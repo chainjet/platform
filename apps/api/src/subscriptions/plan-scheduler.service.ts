a32938dd447190e40968011cd43dc117cb9d7f54
marian2js <marianomdq1@gmail.com>
2023-10-10 12:17:26 +0200

    refactor: reset user operations

--- apps/api/src/subscriptions/plan-scheduler.service.ts
+++ apps/api/src/subscriptions/plan-scheduler.service.ts
@@ -3,6 +3,7 @@ import { Injectable, Logger } from '@nestjs/common'
 import { Interval } from '@nestjs/schedule'
 import { UserService } from 'apps/api/src/users/services/user.service'
 import { plansConfig } from '../users/config/plans.config'
+import { User } from '../users/entities/user'
 import { WorkflowTriggerService } from '../workflow-triggers/services/workflow-trigger.service'
 
 @Injectable()
@@ -27,20 +28,24 @@ export class PlanSchedulerService {
     })
     this.logger.log(`Found ${users.length} users to reset operations`)
     for (const user of users) {
-      await this.userService.updateOneNative(
-        { _id: user._id },
-        {
-          operationsUsedMonth: 0,
-          operationsReset: addOneMonth(user.operationsReset),
-          ...(user.nextPlan ? { plan: user.nextPlan } : {}),
-          $unset: { nextPlan: '' },
-        },
-      )
-      if (user.nextPlan && user.plan !== user.nextPlan) {
-        await this.workflowTriggerSerive.updateFeaturesOnPlanChange(user._id, plansConfig[user.nextPlan])
-      }
-      await this.workflowTriggerSerive.unmarkUserPlanAsLimited(user._id)
-      this.logger.log(`Reset operations for user ${user._id}`)
+      await this.resetOperationsForUser(user)
     }
+  }
+
+  async resetOperationsForUser(user: User): Promise<void> {
+    await this.userService.updateOneNative(
+      { _id: user._id },
+      {
+        operationsUsedMonth: 0,
+        operationsReset: addOneMonth(user.operationsReset),
+        ...(user.nextPlan ? { plan: user.nextPlan } : {}),
+        $unset: { nextPlan: '' },
+      },
+    )
+    if (user.nextPlan && user.plan !== user.nextPlan) {
+      await this.workflowTriggerSerive.updateFeaturesOnPlanChange(user._id, plansConfig[user.nextPlan])
+    }
+    await this.workflowTriggerSerive.unmarkUserPlanAsLimited(user._id)
+    this.logger.log(`Reset operations for user ${user._id}`)
   }
 }
